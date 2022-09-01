448a8bd70defba45b3c09349a800cc5ce37431b7
Misha Causur <mishacausur@mail.ru>
2022-09-01 23:20:19 +0500

    storyboards removed

--- homeleader/MainViewController.m
+++ homeleader/MainViewController.m
@@ -5,13 +5,13 @@
 //  Created by Misha Causur on 01.09.2022.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@interface ViewController ()
+@interface MainViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
