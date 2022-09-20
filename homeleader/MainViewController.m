a553ae5b970bfe0e2ac9e2fa81a16171a0e11cd7
Misha Causur <mishacausur@mail.ru>
2022-09-21 00:08:03 +0500

    still in progress

--- homeleader/MainViewController.m
+++ homeleader/MainViewController.m
@@ -12,6 +12,7 @@
 @property (nonatomic, retain) UICollectionView * collectionView;
 @property (nonatomic, retain) UICollectionViewFlowLayout * layout;
 @end
+
 typedef void(^Completion)(void);
 
 @implementation MainViewController
@@ -22,8 +23,20 @@ typedef void(^Completion)(void);
     NSCollectionLayoutSection * _Nullable(^sectionProvider)(NSInteger, id<NSCollectionLayoutEnvironment> _Nonnull) = ^NSCollectionLayoutSection * _Nullable (NSInteger sectionIndex, id<NSCollectionLayoutEnvironment> _Nonnull layoutEnvironment) {
         return nil;
     };
+    UICollectionViewCompositionalLayoutConfiguration * configuration = [[UICollectionViewCompositionalLayoutConfiguration alloc] init];
+    self->_layout = [[UICollectionViewCompositionalLayout alloc] initWithSectionProvider:sectionProvider configuration:configuration];
+    [self.view addSubview:_collectionView];
+    _collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:_layout];
+    _collectionView.translatesAutoresizingMaskIntoConstraints = false;
     
+    [_collectionView.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
+    [_collectionView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
+    [_collectionView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
+    [_collectionView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
     
+    _collectionView.delegate = self;
+    _collectionView.dataSource = self;
+    [_collectionView registerClass:UICollectionView.class forCellWithReuseIdentifier:@"CellID"];
 }
 
 Completion completion = ^(void) {
